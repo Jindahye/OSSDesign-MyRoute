96609674430953b7110df8af0074c4ff95a432ce
Jindahye <jindahye0315_@naver.com>
2026-06-11 15:06:44 +0900

    fix: 최신 라우터 문법(path-to-regexp) 에러 수정

--- backend/server.js
+++ backend/server.js
@@ -79,7 +79,7 @@ app.use('/api/reports', routeRoutes);
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
